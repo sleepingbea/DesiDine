382e477ff6ec20ee83e8dbdf1443032952668ab0
khushiagarwal <1234khushiagarwal@gmail.com>
2025-07-15 12:58:29 +0000

    Desidine- just made some changes and completed the project

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const  connectDB = async () =>{
+
+    await mongoose.connect('mongodb://127.0.0.1:27017/DesiDine').then(()=>console.log("DB Connected"));
+   
+}
+
+
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+        <img src={assets.logo} alt="" style={{ height: '200px', width:300 }} />
+            
+            <div className="footer-social-icon">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>91-6969696969</li>
+                <li>desidine@gmail.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Desidine.com 
+      </p>
+    </div>
+  )
+}
+import './Footer.css'
+
+export default Footer
